8b0ccc0ecdc8c6c324c7bf4ad29b5abe9736a605
Josephine Halkola Persson <50661752+josiehpersson@users.noreply.github.com>
2021-04-01 13:50:20 +0200

    Footer (#20)
    * Initail Footer pushed to Footer
    
    * Fixed styling
    
    Co-authored-by: pontus sidlo <poppe9898@hotmail.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Footer/>
     </div>
   );
 }
