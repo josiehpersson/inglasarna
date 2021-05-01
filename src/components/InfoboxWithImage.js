86cdfa8cf10b88484fdd3cd334e908e7d76c0b7e
josiehpersson <josephine.h.persson@outlook.com>
2021-05-01 10:56:51 +0200

    added parallax

--- src/components/InfoboxWithImage.js
+++ src/components/InfoboxWithImage.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Grid, Typography, makeStyles } from '@material-ui/core';
+import { Parallax } from 'react-parallax';
 import BackgroundImage from '../img/lumon-balkonginglasning-25.jpg';
 
 const useStyles = makeStyles((theme) => ({
   container: {
+    /* 
     backgroundImage: `url(${BackgroundImage})`,
     backgroundSize: '100%',
-    backgroundPosition: 'bottom center',
+    backgroundPosition: 'bottom center', */
     height: 500,
     display: 'flex',
     alignItems: 'center',
@@ -29,22 +31,24 @@ const InfoboxWithImage = () => {
   const classes = useStyles();
   return (
     <Grid container spacing={2} xs={10} className={classes.container}>
-      <Grid container spacing={2} xs={7} className={classes.glas}>
-        <Grid item xs={7}>
-          <Typography variant="h1">RUBRIK</Typography>
+      <Parallax bgImage={BackgroundImage} strength={-200}>
+        <Grid container spacing={2} xs={7} className={classes.glas}>
+          <Grid item xs={7}>
+            <Typography variant="h1">RUBRIK</Typography>
+          </Grid>
+          <Grid item xs={7}>
+            <Typography variant="body1">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac
+              justo commodo, malesuada odio vitae, euismod ante. Mauris quis sem
+              laoreet, lacinia neque eget, venenatis ligula. Maecenas pharetra
+              tincidunt sapien vel mattis. Fusce in faucibus nunc. Integer et
+              velit pulvinar, ultrices odio interdum, mattis tellus. Duis nisl
+              orci, porta vel nisi sit amet, sollicitudin consequat est. Aenean
+              fermentum id arcu in suscipit.
+            </Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={7}>
-          <Typography variant="body1">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac
-            justo commodo, malesuada odio vitae, euismod ante. Mauris quis sem
-            laoreet, lacinia neque eget, venenatis ligula. Maecenas pharetra
-            tincidunt sapien vel mattis. Fusce in faucibus nunc. Integer et
-            velit pulvinar, ultrices odio interdum, mattis tellus. Duis nisl
-            orci, porta vel nisi sit amet, sollicitudin consequat est. Aenean
-            fermentum id arcu in suscipit.
-          </Typography>
-        </Grid>
-      </Grid>
+      </Parallax>
     </Grid>
   );
 };
